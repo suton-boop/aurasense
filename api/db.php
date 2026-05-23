698b1bd1724f5e175c62ac53211aa5a593540ee2
Suton <sutonommp@gmail.com>
2026-05-23 17:29:34 +0800

    Updates

--- api/db.php
+++ api/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost'; // Nanti ubah saat di Hostinger
-$db   = 'u122685751_aurasense'; // Ubah sesuai nama database di Hostinger
-$user = 'u122685751_aurasense'; // Ubah sesuai user database di Hostinger
-$pass = '@Talita31102014#'; // Ubah sesuai password database di Hostinger
+$db   = 'aurasense'; // Asumsi db lokal
+$user = 'root'; // Default Laragon
+$pass = ''; // Default Laragon
 
 $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 $options = [
